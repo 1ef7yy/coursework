b28d43882838ce36d95f742604e4cac4f45f33d9
leftyy <ustyantsev.makar@mail.ru>
2024-04-26 15:35:19 +0300

    cart

--- coursework/coursework/Calculator.cs
+++ coursework/coursework/Calculator.cs
@@ -6,8 +6,49 @@ using System.Threading.Tasks;
 
 namespace coursework
 {
+
+    public struct Item
+    {
+        public int Id;
+        public string Name;
+        public int Price;
+    }
     public class Calculator
     {
-        
+        public List<Item> cart;
+
+
+        public void AddToCart(Item item)
+        {
+            cart.Add(item);
+        }
+
+        public void RemoveFromCart(Item item)
+        {
+            cart.Remove(item);
+        }
+
+        public void Clear()
+        {
+            cart.Clear();
+        }
+
+
+        public int getSum()
+        {
+            int sum = 0;
+
+
+            foreach (var item in cart)
+            {
+                sum += item.Price;
+            }
+
+            return sum;
+        }
+
+
     }
+
+    
 }
